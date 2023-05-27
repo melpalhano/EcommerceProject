536b6e6622627bcdfec73eb112670af374ef6183
Melissa Palhano <mlssplhn@gmail.com>
2023-05-27 18:35:02 -0300

    Função create_produto

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from typing import Union
-
 from fastapi import FastAPI
+from database import Base, engine, Produto
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+class ProdutoRequest(BaseModel):
+    nome: str
+    quantidade: int
+    valor: int
+
+
+Base.metadata.create_all(engine)
 
 app = FastAPI()
 
@@ -9,8 +18,14 @@ def root():
     return "produto"
 
 @app.post("/produto")
-def create_produto():
-    return "create produto"
+def create_produto(produto: ProdutoRequest):
+    session = Session(bind=engine, expire_on_commit=False)
+    produto = Produto(nome=produto.nome, quantidade=produto.quantidade, valor=produto.valor)
+    session.add(produto)
+    session.commit()
+    id = produto.id
+    session.close()
+    return {"id": id, "nome": produto.nome}
 
 @app.get("/produto/{id}")
 def read_produto(id: int):
