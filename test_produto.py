d6d47a18185ae679198ef59d656fddcef844ff9d
andersonpog <silveira.anderson.1988@gmail.com>
2023-06-11 15:27:15 -0300

    Teste delete

--- test_produto.py
+++ test_produto.py
@@ -13,11 +13,27 @@ def sample_produto():
         {"nome": "Produto 5", "quantidade": 50, "valor": 500},
     ]
 
+@pytest.fixture
+def sample_id():
+    return [
+        {"id": 1},
+        {"id": 2},
+        {"id": 3},
+        {"id": 4},
+        {"id": 5},
+    ]
+
 def test_create_produto(sample_produto):
     client = TestClient(app)
     response = client.post("/produto", json=sample_produto[0])
     assert response.status_code == 200
     assert response.json()["nome"] == sample_produto[0]["nome"]
+
+def teste_detele_produto(sample_id):
+    client = TestClient(app)
+    response = client.delete("/produto/"+ str(sample_id[0]))
+    assert response.status_code == 200
+    # assert response.json()["nome"] == sample_produto[0]["nome"]
 
 
 
